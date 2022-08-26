66e3bc33df66598bf2eb9f50dd1bba3b3d4decb4
keishino00 <shin.enginee@gmail.com>
2022-08-26 11:20:01 +0900

    init

--- contracts/MagomintPayment.sol
+++ contracts/MagomintPayment.sol
@@ -7,48 +7,49 @@ import "@openzeppelin/contracts/utils/Address.sol";
 contract MagoMintPayment is Ownable {
     uint256 private constant MAX_SUPPLY = 300;
     uint256 private constant PRIVATE_PRICE = 0.55 ether;
-    uint256 private publicPrice;
 
+    uint256 private publicPrice;
+    uint256 public totalSupply = 0;
     bool public presale = true;
     mapping(address => bool) public whitelist;
     mapping(address => uint256) public purchasedQuantity;
 
     constructor() {}
 
-    event PaymentCompleted(string _puchaseType, address _from, uint256 _amount);
-
-    function publicPurchase(uint256 amount) public payable {
+    function publicPurchase(uint256 _amount) public payable {
         require(!presale, "presale is active");
-        require(msg.value >= amount * publicPrice, "not enough funds");
-        purchasedQuantity[msg.sender] += amount;
-        emit PaymentCompleted("PUBLIC", msg.sender, amount);
+        require(totalSupply + _amount > MAX_SUPPLY, "MAXSUPPLY over");
+        require(msg.value == _amount * publicPrice, "not correct funds");
+        purchasedQuantity[msg.sender] += _amount;
+        totalSupply += _amount;
     }
 
-    function prePurchase(uint256 amount) public payable {
+    function prePurchase(uint256 _amount) public payable {
         require(presale, "presale is not active");
-        require(msg.value >= amount * PRIVATE_PRICE, "not enough funds");
-        purchasedQuantity[msg.sender] += amount;
-        emit PaymentCompleted("PRIVATE", msg.sender, amount);
+        require(totalSupply + _amount > MAX_SUPPLY, "MAXSUPPLY over");
+        require(msg.value == _amount * PRIVATE_PRICE, "not correct funds");
+        purchasedQuantity[msg.sender] += _amount;
+        totalSupply += _amount;
     }
 
     function freePurchase() public {
         require(whitelist[msg.sender], "Already claimed max");
         whitelist[msg.sender] = false;
         purchasedQuantity[msg.sender] += 1;
-        emit PaymentCompleted("FREE", msg.sender, 1);
+        totalSupply += 1;
     }
 
-    function setPublicPrice(uint256 newPublicPrice) public onlyOwner {
-        publicPrice = newPublicPrice;
+    function setWhitelist(address[] calldata _addresses) public onlyOwner {
+        for(uint index=0; index<_addresses.length; index++) {
+            whitelist[_addresses[index]] = true;
+        }
+    }
+
+    function setPublicPrice(uint256 _publicPrice) public onlyOwner {
+        publicPrice = _publicPrice;
     }
 
     function withdraw() external onlyOwner {
         Address.sendValue(payable(0xeC2C16A4aBD441ef48e1b48D644330302F010923), address(this).balance);
-    }
-
-    function setWhitelist(address[] calldata addresses) public onlyOwner {
-        for(uint index=0; index<addresses.length; index++) {
-            whitelist[addresses[index]] = true;
-        }
     }
 }
